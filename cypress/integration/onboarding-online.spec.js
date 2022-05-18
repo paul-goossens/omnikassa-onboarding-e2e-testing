e343ac19f992dc0e404be41e6ba39053fde19722
Paul Goossens <paul.goossens@rabobank.nl>
2022-05-18 14:23:20 +0200

    feat: added test script

--- cypress/integration/onboarding-online.spec.js
+++ cypress/integration/onboarding-online.spec.js
@@ -1,6 +1,27 @@
-// onboarding-online.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+describe('OmniKassa Onboarding Flow - Online', () => {
+  it('Login to onboarding flow', () => {
+    cy.visit('https://omnikassa-edge-router-stub.apps.pcf-t02-we.rabobank.nl/omnikassa-dashboard-login/')
+
+    cy.get('#login').type('322425').should('have.value', '322425')
+
+    cy.get('#target').select('OmniKassa Onboarding').should('have.value', 'onboarding')
+
+    cy.wait(5000)
+
+    cy.get('button').contains('Login').click()
+
+    cy.url().should('include', '/onboarding/start-rabo-omnikassa')
+
+    cy.wait(5000)
+
+    cy.visit('https://omnikassa-edge-router-stub.apps.pcf-t02-we.rabobank.nl/omnikassa/onboarding/start/online')
+
+    cy.url().should('include', '/onboarding/start/online/1')
+
+    cy.wait(5000)
+  })
+
+  it('Fill in the onboarding form', () => {
+    cy.get('input[placeholder="Handelsnaam of merknaam"]').type('{selectall}{backspace}OmniKassa Merchant made by Cypress').should('have.value', 'OmniKassa Merchant made by Cypress')
+  })
+})
